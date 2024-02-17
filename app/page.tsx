0be9344d24afa64983e20e32f435a526586f2eab
Trend20 <enockomondi305@gmail.com>
2024-02-18 00:28:18 +0300

    categories section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Category from "@/components/Category";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col">
       <Hero />
+      <Category />
     </main>
   );
 }
---
+++ components/Category.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+import { TbCategoryPlus } from "react-icons/tb";
+
+const Category = () => {
+  return (
+    <div className="flex w-full flex-col justify-center items-center">
+      <h1 className="flex text-2xl w-1/2 justify-center items-center font-extrabold leading-10- text-boxdark-2">
+        Browse by Category
+      </h1>
+      <p className="flex text-lg text-center mt-3 w-1/2">
+        From small NPM modules to fully functional libraries, we give you access
+        a wide list of tools that can help you get on speed building and
+        shipping your application.
+      </p>
+      <Link
+        href="/login"
+        className="flex w-50 mt-3 bg-boxdark-2 text-sm uppercase text-white justify-center items-center p-3 rounded-md"
+      >
+        <i className="mr-3">
+          <TbCategoryPlus />
+        </i>
+        Categories
+      </Link>
+    </div>
+  );
+};
+
+export default Category;
