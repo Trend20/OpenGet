684cbeb4f8c9143fd6fbb01f95a675d8438a0259
Trend20 <enockomondi305@gmail.com>
2024-02-18 14:00:53 +0300

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,18 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Login = () => {
   return (
     <div className="flex  m-auto justify-center items-center flex-col py-10 px-10 mt-20 w-[270] bg-bodydark1 rounded-md">
-      <div className="flex w-20 justify-start items-center">
+      <div className="flex flex-col justify-center items-center">
         <Link href="/" className="flex font-bold text-2xl items-center">
-          <Image
-            src="logo.svg"
-            alt="logo"
-            width="100"
-            height="100"
-            className="w-3/4"
-          />
+          <Image src="logo.svg" alt="logo" width="100" height="100" />
         </Link>
+        <h1 className="text-2xl font-bold text-black-2">Welcome</h1>
+        <p>Sign in to your account!</p>
       </div>
       <div className="flex flex-col mt-10 w-full">
         <Link
@@ -32,7 +30,8 @@ const Login = () => {
           </div>
         </Link>
         <Link
-          href="/"
+          href="#"
+          onClick={() => signIn("github")}
           className="flex mt-5 rounded-md p-5 bg-whiten items-center"
         >
           <div className="flex w-10">
---
+++ next-auth.config.ts
@@ -0,0 +1,20 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+const options: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    // Add other providers as needed
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+  },
+};
+
+export default options;
