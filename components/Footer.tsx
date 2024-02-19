e57ae73605d84f96c700d611d2a57168fddcb1c4
Trend20 <enockomondi305@gmail.com>
2024-02-19 13:29:53 +0300

    ui fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,15 +3,23 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex w-full mt-20 flex-col justify-center items-center max-md:w-full max-md:flex-col">
-      <p className="text-sm max-md:p-1">© {new Date().getFullYear()} OpenGet</p>
-      <p className="text-sm max-md:p-1">All Rights Reserved</p>
-      <p className="text-sm max-md:p-1">
-        Created with ♥ from Nairobi by{" "}
-        <Link href="https://github.com/Trend20" target="_blank">
-          Dev Enock
-        </Link>
-      </p>
+    <div className="flex w-full mt-25 flex-col justify-center items-center max-md:w-full max-md:flex-col">
+      <div className="flex w-1/4 justify-between">
+        {/* <p className="text-sm max-md:p-1">All Rights Reserved</p> */}
+        <p className="text-sm max-md:p-1">
+          © {new Date().getFullYear()} OpenGet
+        </p>
+        <p className="text-sm max-md:p-1 flex">
+          Created with ♥ from Nairobi by{" "}
+          <Link
+            href="https://github.com/Trend20"
+            className="flex ml-3 text-meta-5"
+            target="_blank"
+          >
+            Dev Enock
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
