4b0eca501cf73a867456d75d9edb668657f48793
Trend20 <enockomondi305@gmail.com>
2024-02-22 11:05:14 +0300

    adding error and loading UI

--- app/packages/[id]/page.tsx
+++ app/packages/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { getPlatformByName } from "@/app/api/platforms";
+import { getPlatformByName } from "@/app/api/packages";
 import React from "react";
 
 const Platform = async ({ params }: { params: { name: string } }) => {
   const name = params.name;
-  const platform = await getPlatformByName(name);
-  console.log(platform);
+  // const platform = await getPlatformByName(name);
+  // console.log(platform);
   return <div>Platform</div>;
 };
 
