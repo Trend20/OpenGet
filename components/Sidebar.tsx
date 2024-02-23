0cc12f813bfe878d8d72d46009c06872aa625eb3
Trend20 <enockomondi305@gmail.com>
2024-02-24 01:56:07 +0300

    sidebar fix

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -60,39 +60,37 @@ const languages: ILanguages[] = [
 const Sidebar: React.FC = () => {
   const [clicked, setClicked] = useState("javascript");
   return (
-    <>
-      <div className="flex w-full items-start">
-        <ul className="flex-col  w-1/4 border border-grey rounded-md text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
-          {languages.map((lang) => (
-            <li
-              key={lang.id}
-              className={`flex w-full border-b border-b-grey ${
-                clicked ? "tabActive" : ""
-              }`}
+    <div className="flex w-full items-start">
+      <ul className="flex-col  w-1/4 border border-grey rounded-md text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
+        {languages.map((lang) => (
+          <li
+            key={lang.id}
+            className={`flex w-full border-b border-b-grey ${
+              clicked ? "tabActive" : ""
+            }`}
+          >
+            <button
+              onClick={() => setClicked(lang.name)}
+              className="flex justify-center items-center px-4 py-4 text-white rounded-lg active w-full bg-blue-gray-50"
+              aria-current="page"
             >
-              <button
-                onClick={() => setClicked(lang.name)}
-                className="flex justify-center items-center px-4 py-4 text-white rounded-lg active w-full bg-blue-gray-50"
-                aria-current="page"
-              >
-                <div className="flex w-8 justify-center items-center">
-                  <Image
-                    src={lang.icon}
-                    alt={lang.name}
-                    width="100"
-                    height="100"
-                    className="w-full flex"
-                  />
-                </div>
-              </button>
-            </li>
-          ))}
-        </ul>
-        <div className="flex border border-grey rounded-md w-3/4">
-          <Languages language={clicked} />
-        </div>
+              <div className="flex w-8 justify-center items-center">
+                <Image
+                  src={lang.icon}
+                  alt={lang.name}
+                  width="100"
+                  height="100"
+                  className="w-full flex"
+                />
+              </div>
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="flex border border-grey rounded-md w-3/4">
+        <Languages language={clicked} />
       </div>
-    </>
+    </div>
   );
 };
 
