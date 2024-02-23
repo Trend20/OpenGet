ae37c440c4fa2879c711d26464e7966d85d2a71d
Trend20 <enockomondi305@gmail.com>
2024-02-23 16:26:26 +0300

    github authentication

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,16 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GrProjects } from "react-icons/gr";
 import { TbPackages } from "react-icons/tb";
-import { usePathname } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
+import { useSession, signOut } from "next-auth/react";
+// import { signIn } from "next-auth/react";
+import { LiaCaretDownSolid } from "react-icons/lia";
+import { FaRegUser } from "react-icons/fa";
+import { TbLogout } from "react-icons/tb";
+import router from "next/router";
 
 const headerLinks = [
   {
@@ -37,6 +43,13 @@ const headerLinks = [
 
 const Header = () => {
   const pathname = usePathname();
+  const [showDiv, setShowDiv] = useState(false);
+  const { data: session } = useSession();
+
+  if (!session) {
+    if (session) return redirect("/projects");
+  }
+
   return (
     <div className="flex w-full items-center justify-between py-10">
       <div className="flex w-25.5 justify-start items-center">
@@ -65,12 +78,56 @@ const Header = () => {
             {link.linkName}
           </Link>
         ))}
-        <Link
-          href="/login"
-          className="flex w-40 bg-boxdark-2 text-white justify-center items-center p-4 rounded-md"
-        >
-          Get Started
-        </Link>
+        {!session ? (
+          <Link
+            href="/login"
+            className="flex w-40 bg-boxdark-2 text-white justify-center items-center p-4 rounded-md"
+          >
+            Get Started
+          </Link>
+        ) : (
+          <div className="flex cursor-pointer flex-col">
+            <div
+              className="flex items-center cursor-pointer"
+              onClick={() => setShowDiv((prev) => !prev)}
+            >
+              <Image
+                src={session.user?.image}
+                alt="Avatar"
+                width="100"
+                height="100"
+                className="flex rounded-full w-10.5 h-10.5"
+              />
+              <i className="px-3">
+                <LiaCaretDownSolid />
+              </i>
+            </div>
+            {showDiv ? (
+              <div className="flex flex-col absolute bg-blue-gray-50 top-20 px-3 py-5 h-25.5 rounded-md items-start- justify-start">
+                <Link
+                  href="/profile"
+                  className="flex items-center rounded-md p-1 hover:bg-meta-4 hover:text-whiten"
+                >
+                  <i className="mr-3">
+                    <FaRegUser />
+                  </i>
+                  {session.user?.name}
+                </Link>
+                <button
+                  onClick={() => signOut({ redirect: true })}
+                  className="flex items-center p-1 rounded mt-5 hover:bg-meta-4 hover:text-whiten"
+                >
+                  <i className="mr-3">
+                    <TbLogout />
+                  </i>
+                  Logout
+                </button>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
