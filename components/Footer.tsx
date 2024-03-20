0f17965c2faa69064fca67db17b225597d1a555f
Trend20 <enockomondi305@gmail.com>
2024-03-20 20:41:37 +0300

    footer fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
             href="#"
           >
             <FaDiscord className="h-4 w-4" />
-            <span className="sr-only">Facebook</span>
+            <span className="sr-only">Discord</span>
           </Link>
           <Link
             className="rounded-full hover:bg-gray-100 p-2 transition-colors text-gray-500 dark:hover:bg-gray-800 dark:text-gray-400"
@@ -26,7 +26,7 @@ const Footer = () => {
             href="#"
           >
             <FaGithub className="h-4 w-4" />
-            <span className="sr-only">Instagram</span>
+            <span className="sr-only">GitHub</span>
           </Link>
         </div>
         <nav className="flex items-center space-x-4">
@@ -44,34 +44,16 @@ const Footer = () => {
           </Link>
           <Link
             className="font-medium transition-colors hover:text-gray-900 dark:hover:text-gray-50"
-            href="#"
+            href="mailto:enockomondi305@gmail.com"
           >
             Contact Us
           </Link>
         </nav>
         <p className="text-gray-500 text-xs tracking-wider dark:text-gray-400">
-          © 2024 Acme Inc. All rights reserved.
+          © {new Date().getFullYear()} OpenGet. All rights reserved.
         </p>
       </div>
     </footer>
-    // <div className="flex bg-black w-full py-8 flex-col justify-center items-center max-md:w-full max-md:flex-col">
-    //   <div className="flex w-1/4 justify-between">
-    //     {/* <p className="text-sm max-md:p-1">All Rights Reserved</p> */}
-    //     <p className="text-sm max-md:p-1">
-    //       © {new Date().getFullYear()} OpenGet
-    //     </p>
-    //     <p className="text-sm max-md:p-1 flex">
-    //       Created with ♥ from Nairobi by{" "}
-    //       <Link
-    //         href="https://github.com/Trend20"
-    //         className="flex ml-3 text-meta-5"
-    //         target="_blank"
-    //       >
-    //         Dev Enock
-    //       </Link>
-    //     </p>
-    //   </div>
-    // </div>
   );
 };
 
