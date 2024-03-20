ab636912efbfc6fed6bce126a9d78f0094dcb0d7
Trend20 <enockomondi305@gmail.com>
2024-03-20 18:55:55 +0300

    hero text fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,11 +7,11 @@ export default function Hero() {
   return (
     <div className="flex w-full flex-col justify-center m-auto items-center py-40 px-40 mt-30">
       <h1 className="text-7xl text-center w-3/4 font-extrabold py-5 leading-[1.08] text-boxdark-2 mt-10">
-        Discover amazing open-source projects
+        Explore the World of Open Source
       </h1>
       <p className="text-lg text-center w-1/2">
-        Welcome to the world of OpenSource Software! Explore the best
-        open-source projects. Contribute, star, and build amazing things.
+        Discover a collection of open source projects, inspiring stories, and
+        insightful podcasts. Contribute, star, and build amazing things.
       </p>
       <div className="flex mt-10">
         <div className="px-3">
