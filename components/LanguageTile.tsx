3ca31e932e8f5a428daa124a3db5955c6f024b63
Trend20 <enockomondi305@gmail.com>
2024-03-21 11:23:10 +0300

    responsive UI

--- components/LanguageTile.tsx
+++ components/LanguageTile.tsx
@@ -9,9 +9,68 @@ import { FaGithub } from "react-icons/fa6";
 
 const LanguageTile = ({ repo }: any) => {
   return (
-    <div
-      className={`flex flex-col rounded-md border cursor-pointer border-grey`}
-    >
+    // <div
+    //   className={`flex flex-col rounded-md border cursor-pointer border-grey`}
+    // >
+    //   <div className="flex p-3 items-center justify-between">
+    //     <div className="flex items-center">
+    //       <div className="flex bg-blue-gray-100 p-2 rounded-full">
+    //         <Image
+    //           src={repo.owner.avatar_url}
+    //           alt={repo.name}
+    //           width="100"
+    //           height="100"
+    //           className="flex w-10.5 rounded-full"
+    //         />
+    //       </div>
+    //       <h3 className="font-bold flex border-none ml-2">{repo.name}</h3>
+    //     </div>
+    //     <div className="flex space-x-3">
+    //       {repo.homepage !== "" && repo.homepage !== null ? (
+    //         <Link href={repo.homepage} target="_blank">
+    //           <GoGlobe color="#0394fc" size={20} />
+    //         </Link>
+    //       ) : (
+    //         <Link href={repo.html_url} target="_blank">
+    //           <GoGlobe color="#0394fc" size={20} />
+    //         </Link>
+    //       )}
+    //       <Link href={repo.html_url} target="_blank">
+    //         <FaGithub color="#0394fc" size={20} />
+    //       </Link>
+    //     </div>
+    //   </div>
+    //   <div className="flex p-3 items-center justify-between">
+    //     <p>{repo.description}</p>
+    //   </div>
+    //   <hr className="border-grey" />
+    //   <div className="flex p-3 items-center justify-between">
+    //     <p>Language:</p> <p>{repo.language}</p>
+    //   </div>
+    //   <hr className="border-grey" />
+    //   <div className="flex p-3 items-center justify-between">
+    //     <p>
+    //       <MdOutlineStar color="#0394fc" size={20} />
+    //     </p>{" "}
+    //     <p>{repo.stargazers_count}</p>
+    //   </div>
+    //   <hr className="border-grey" />
+    //   <div className="flex p-3 items-center justify-between">
+    //     {" "}
+    //     <p>
+    //       <GoIssueOpened color="#0394fc" size={20} />
+    //     </p>{" "}
+    //     <p>{repo.open_issues_count}</p>
+    //   </div>
+    //   <hr className="border-grey" />
+    //   <div className="flex p-3 items-center justify-between">
+    //     <p>
+    //       <PiGitForkBold color="#0394fc" size={20} />
+    //     </p>{" "}
+    //     <p>{repo.forks_count}</p>
+    //   </div>
+    // </div>
+    <div className="flex flex-col rounded-md border cursor-pointer border-grey">
       <div className="flex p-3 items-center justify-between">
         <div className="flex items-center">
           <div className="flex bg-blue-gray-100 p-2 rounded-full">
@@ -51,22 +110,21 @@ const LanguageTile = ({ repo }: any) => {
       <div className="flex p-3 items-center justify-between">
         <p>
           <MdOutlineStar color="#0394fc" size={20} />
-        </p>{" "}
+        </p>
         <p>{repo.stargazers_count}</p>
       </div>
       <hr className="border-grey" />
       <div className="flex p-3 items-center justify-between">
-        {" "}
         <p>
           <GoIssueOpened color="#0394fc" size={20} />
-        </p>{" "}
+        </p>
         <p>{repo.open_issues_count}</p>
       </div>
       <hr className="border-grey" />
       <div className="flex p-3 items-center justify-between">
         <p>
           <PiGitForkBold color="#0394fc" size={20} />
-        </p>{" "}
+        </p>
         <p>{repo.forks_count}</p>
       </div>
     </div>
--- components/skelletons/PackageSkeleton.tsx
+++ components/skelletons/PackageSkeleton.tsx
@@ -3,24 +3,41 @@ import Link from "next/link";
 
 const PackageSkeleton = () => {
   return (
-    <div
-      className={`flex flex-col rounded-md border cursor-pointer h-67 p-5 border-grey animate-pulse`}
-    >
+    // <div
+    //   className={`flex flex-col rounded-md border cursor-pointer h-67 p-5 border-grey animate-pulse`}
+    // >
+    //   <div className="flex p-3 items-center h-10 justify-between rounded-md bg-blue-gray-300">
+    //     <h3 className="font-bold flex border-none"></h3>
+    //   </div>
+    //   <div className="flex p-3 mt-8  h-10 items-center justify-between rounded-md bg-blue-gray-300">
+    //     <p>{}</p>
+    //   </div>
+
+    //   <div className="flex p-3 mt-8 h-10 items-center justify-between rounded-md bg-blue-gray-300">
+    //     <p>{}</p>
+    //   </div>
+
+    //   <div className="flex py-5 mt-8">
+    //     <Link
+    //       href={``}
+    //       className="flex justify-center bg-blue-gray-300 h-10 items-center rounded-md p-3 w-3/4 m-auto"
+    //     ></Link>
+    //   </div>
+    // </div>
+    <div className="flex flex-col rounded-md border cursor-pointer h-auto sm:h-67 p-5 border-grey animate-pulse">
       <div className="flex p-3 items-center h-10 justify-between rounded-md bg-blue-gray-300">
         <h3 className="font-bold flex border-none"></h3>
       </div>
-      <div className="flex p-3 mt-8  h-10 items-center justify-between rounded-md bg-blue-gray-300">
+      <div className="flex p-3 mt-3 sm:mt-8 h-10 items-center justify-between rounded-md bg-blue-gray-300">
         <p>{}</p>
       </div>
-
-      <div className="flex p-3 mt-8 h-10 items-center justify-between rounded-md bg-blue-gray-300">
+      <div className="flex p-3 mt-3 sm:mt-8 h-10 items-center justify-between rounded-md bg-blue-gray-300">
         <p>{}</p>
       </div>
-
-      <div className="flex py-5 mt-8">
+      <div className="flex py-3 sm:py-5 mt-3 sm:mt-8">
         <Link
           href={``}
-          className="flex justify-center bg-blue-gray-300 h-10 items-center rounded-md p-3 w-3/4 m-auto"
+          className="flex justify-center bg-blue-gray-300 h-10 items-center rounded-md p-3 w-3/4 sm:w-full m-auto"
         ></Link>
       </div>
     </div>
