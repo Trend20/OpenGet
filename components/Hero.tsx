f458c6dd7d64437242ff565d2449cb4c94d0699b
Trend20 <enockomondi305@gmail.com>
2024-03-21 10:27:17 +0300

    responsive hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,15 +5,15 @@ import { MdOutlineExplore } from "react-icons/md";
 
 export default function Hero() {
   return (
-    <div className="flex w-full flex-col justify-center m-auto items-center py-20 px-40 mt-20">
-      <h1 className="text-7xl text-center w-3/4 font-extrabold py-5 leading-[1.08] text-boxdark-2 mt-10">
+    <div className="flex flex-col justify-center items-center py-10 md:py-20 px-6 md:px-40 mt-10 md:mt-20">
+      <h1 className="text-4xl md:text-7xl text-center w-full md:w-3/4 font-extrabold py-5 leading-[1.08] text-boxdark-2 mt-5 md:mt-10">
         Explore the World of Open Source
       </h1>
-      <p className="text-lg text-center w-1/2">
+      <p className="text-base md:text-lg text-center w-full md:w-1/2">
         Discover a collection of open source projects, inspiring stories, and
         insightful podcasts. Contribute, star, and build amazing things.
       </p>
-      <div className="flex mt-10">
+      <div className="flex mt-8 md:mt-10">
         <div className="px-3">
           <Link
             href="/explore"
