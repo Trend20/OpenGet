bc3dcbee727af1c9b88b42c297edef23dc0bd345
Trend20 <enockomondi305@gmail.com>
2024-03-22 18:12:39 +0300

    adding news and events

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -6,29 +6,17 @@ import { BiUpvote } from "react-icons/bi";
 const NewsCard = ({ story }: any) => {
   return (
     <div className="flex flex-col sm:flex-row items-center rounded-md border border-grey p-3 justify-between w-full">
-      <div className="flex flex-col sm:flex-row">
-        <Image
-          loading="lazy"
-          src={story.urlToImage}
-          alt="logo"
-          width="50"
-          height="50"
-          className="flex w-8.5"
-        />
+      <div className="flex flex-col">
         <h3 className="leading-8 font-bold text-xl py-3 sm:pr-4">
           {story.title}
         </h3>
-        <h3 className="leading-8 font-bold text-xl py-3 sm:pr-4">
-          {story.description}
-        </h3>
+        <h3 className="text-normal py-3 sm:pr-4">{story.description}</h3>
         <p className="leading-8 py-3 flex items-center font-bold">
           <i className="rounded-full p-2 border border-meta-5 mr-3">
             <BiUpvote size={15} fill="#0394fc" />
           </i>{" "}
           {story.score}
         </p>
-      </div>
-      <div className="flex justify-center sm:justify-end items-center mt-3 sm:mt-0">
         <Link
           href={story.url}
           target="_blank"
